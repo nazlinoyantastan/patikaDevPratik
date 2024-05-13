0bd31cc460c4d9262b09d8380295014a65b546f5
nazlinoyantastan <nazlinoyan_88@hotmail.com>
2024-05-13 12:00:21 +0300

    ortalama

--- src/main/java/com/tobeto/Main.java
+++ src/main/java/com/tobeto/Main.java
@@ -1,17 +1,42 @@
 package com.tobeto;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       // notOrtalamasi();
+        kdvTutari();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    private static void notOrtalamasi() {
+        int mat, fizik, kimya, turkce;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Matematik notunuz : ");
+        mat = inp.nextInt();
+        System.out.print("Fizik notunuz : ");
+        fizik = inp.nextInt();
+        System.out.print("Kimya notunuz : ");
+        kimya = inp.nextInt();
+        System.out.print("Türkçe notunuz : ");
+        turkce = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce);
+        double sonuc = toplam / 4;
+        System.out.println("Ortalamanız : " + sonuc);
+
+        // // Ternary Operator kullanarak koşullu ifade
+        String durum = (sonuc > 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+
+        System.out.println("Sonuç: " + durum);
+
+
+    }
+
+    private static void kdvTutari (){
+
+
     }
 }
